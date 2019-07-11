da8b4abf510f399e1821611d58803f078a44cc4b
r10rivas <robertorivas@devtohack.com>
2019-07-10 22:43:15 -0400

    Adding convert-units

--- src/components/weatherLocation/index.js
+++ src/components/weatherLocation/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import convert from 'convert-units';
 import Location from './location'
 import WeatherData from './weatherData/'
 import {
@@ -10,18 +11,72 @@ import {
   WINDY
 } from './../../constants/weathers';
 
-const data = {
-  temperature: 20,
-  weatherState: SUN,
-  humidity: 10,
-  wind: '10 m/s',
-}
+const location = 'Buenos Aires,ar'
+const token = 'e9d1e025285c65ad323a174cd42d9d5d'
+const api_url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${token}`;
 
-const WeatherLocation = () => (
-  <div>
-    <Location city={'La capital del mal'}/>
-    <WeatherData data={data}/>
-  </div>
-)
+class WeatherLocation extends Component {
+  // state = {
+  //   data: data,
+  //   city: 'buenos aires',
+  // }
+
+  constructor() {
+    super();
+    this.state = {
+      city: 'buenos aires',
+      data: {
+        temperature: 20,
+        weatherState: SUN,
+        humidity: 10,
+        wind: '10 m/s'
+      }
+    }
+  }
+
+  getWeatherState = weather => {
+    console.log( weather );
+    return RAIN
+  }
+  getTemperature = tempKelvin => {
+    return convert(tempKelvin).from('K').to('C').toFixed(2);
+  }
+
+  getData = data_api => {
+    const {humidity} = data_api.main;
+    const {speed} = data_api.wind;
+    const weatherState = this.getWeatherState(data_api.weather);
+    const temperature = this.getTemperature(data_api.main.temp)
+    return {
+      humidity,
+      temperature: temperature,
+      weatherState,
+      wind: `${speed} m/s`
+    }
+
+  }
+
+  handleUpdateClick = () => {
+    fetch(api_url).then((response) => {
+      return response.json();
+    }).then((data_api) => {
+      const data = this.getData(data_api);
+      this.setState({data})
+    }).catch((error) => {
+      console.error(error);
+    })
+  }
+
+  render = () => {
+    const {city, data}= this.state;
+    return(
+      <div>
+        <Location city={city}/>
+        <WeatherData data={data}/>
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
+      </div>
+    )
+  }
+}
 
 export default WeatherLocation;
--- src/components/weatherLocation/weatherData/weatherExtraInfo.js
+++ src/components/weatherLocation/weatherData/weatherExtraInfo.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 
 const WeatherExtraInfo = ({humidity, wind}) => (
   <div className='weather-extraInfo'>
-    <span className='weather-extraInfo-text'>{`${humidity}%`}</span>
-    <span className='weather-extraInfo-text'>{ `${wind} wind`}</span>
+    <span className='weather-extraInfo-text'>{`Humedad: ${humidity}%`}</span>
+    <span className='weather-extraInfo-text'>{`Vientos: ${wind}`}</span>
   </div>
 )
 
